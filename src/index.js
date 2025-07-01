40ca4fb20a61f83b83fd840aa9dc06f742ce4ee4
Amit <amitgupta561710@gmail.com>
2025-07-01 09:57:52 +0530

    added cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDatabase from './config/db.js';
 import jobRoutes from './routes/jobs.js';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(cors());
 
 connectDatabase();
 
